a9bcf6986d70d7c84682250a78285f0e7eb8ec16
João Sousa <joaopedrosousa1995@gmail.com>
2023-09-29 14:36:59 +0100

    part1update

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,29 +1,81 @@
-import { useState } from "react"
+const Header = ({course}) => {
+  return (
+    <>
+      <h1>{course.name}</h1>
+    </>
+  )
+}
+const Content = (props) => {
+  console.log(props)
+  return (
+    <>
+      <Part1 part1 = { props.parts[0] } />
+      <Part2 part2 = { props.parts[1] } />
+      <Part3 part3 = { props.parts[2] } />
+    </>
+  )
+}
+
+const Part1 = ({ part1 }) => {
+  return (
+    <p>
+      {part1.name} {part1.exercises1}
+    </p>
+  )
+}
+
+const Part2 = ({ part2 }) => {
+  return (
+    <p>
+      {part2.name} {part2.exercises}
+    </p>
+  )
+}
+
+const Part3 = ({ part3 }) => {
+  return (
+    <p>
+      {part3.name} {part3.exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </>
+  )
+}
+
+
 
 const App = () => {
-  const [valor, setValor] = useState(10)
-
-  const setNewValue = (newValue) => {
-    console.log('setValor atual', newValue)
-    setValor(newValue)
+  const course = {
+    name: 'Half Stack App Development',
+    parts: [
+      {
+        name: 'React Library Fundamentals',
+        exercises: 10
+      },
+      {
+        name: 'Passing Data With Props',
+        exercises: 7
+      },
+      {
+        name: 'Component States',
+        exercises: 14
+      }
+    ]
   }
-
 
   return (
     <div>
-      <Exibir valor={valor} />
-      <Botao setNewValue={() => setNewValue(valor + 2000)} text="+2000" />
-      <Botao setNewValue={() => setNewValue(0)} text="Zero" />
-      <Botao setNewValue={() => setNewValue(valor + 1)} text="+1" />
+      <Header course={course} />
+      <Content parts = { course.parts } />
+      <Total parts = { course.parts } />
     </div>
   )
 }
 
-const Exibir = (props) => (
-  <div>{props.valor}</div>
-)
-
-const Botao = ({ setNewValue, text }) => (
-  <button onClick={setNewValue}>{text}</button>
-)
 export default App
